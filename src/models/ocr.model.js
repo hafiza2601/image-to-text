4a87284eab3a038b81ec37199a7e92961ef7ae4a
hafiza <hafizachhipa@gmail.com>
2024-07-06 18:01:22 +0530

    remove unwanted files files

--- src/models/ocr.model.js
+++
@@ -1,7 +0,0 @@
-const Schema = mongoose.Schema;
-const ocrSchema = new Schema({
-    image: String,
-    text: String,
-    boldWords: [String]
-});
-const ocrModel = mongoose.model('Image', ocrSchema);
--- src/routes/ocr.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { uploadImage } from "../controllers/ocr.controller.js";
-import { upload } from "../middleware/multer.middleware.js";
-
-const router = Router()
-
-router.route("/upload").post(upload.single("ocrImage"), uploadImage)
-
-export default router
