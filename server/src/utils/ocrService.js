12b46703872cd300a65ee1a83f8f968563716733
hafiza <hafizachhipa@gmail.com>
2024-07-06 21:43:46 +0530

    Update ocrservice

--- server/src/utils/ocrService.js
+++ server/src/utils/ocrService.js
@@ -3,7 +3,11 @@ import { createWorker } from 'tesseract.js';
 export const extractTextAndBoldWords = async (imageBuffer) => {
     try {
         const worker = await createWorker('eng');
-        const { data: { text } } = await worker.recognize(imageBuffer);
+        console.log("worker", worker)
+        const { data: { text } } = await worker.recognize(imageBuffer, {
+            corePath: 'https://unpkg.com/tesseract.js-core@5.1.0/tesseract-core.wasm.js'
+        });
+        console.log("extracted data", text)
         await worker.terminate();
         return { extractedText: text };
     } catch (error) {
