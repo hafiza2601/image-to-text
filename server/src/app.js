cd643169868079e1c2f4f6ba988e189989be6214
hafiza2601 <58465024+hafiza2601@users.noreply.github.com>
2024-07-07 00:55:09 +0530

    Update CORS

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
+    methods: ['POST', 'GET'],
     credentials: true
 }))
 app.use(express.json({ limit: "16kb" }))
