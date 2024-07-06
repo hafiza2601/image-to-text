1a347c77bcd8ea1d2406f695723ce4b711ec4a04
hafiza <hafizachhipa@gmail.com>
2024-07-07 01:46:07 +0530

    Update apiUrl

--- client/src/services/ImageToText.js
+++ client/src/services/ImageToText.js
@@ -1,12 +1,12 @@
-// src/api.js
 import axios from 'axios';
+const apiUrl = process.env.REACT_APP_API_URL;
 
 class ImageToTextService {
     static async uploadImage(file) {
         try {
             const formData = new FormData();
             formData.append('ocrImage', file);
-            const response = await axios.post('/api/v1/upload', formData);
+            const response = await axios.post(`${apiUrl}/api/v1/upload`, formData);
             console.log("response", response)
             return response?.data?.extractedText;
         } catch (error) {
